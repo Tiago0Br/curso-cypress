802eafd247b4dd244ec0010a726b6b5e3aca1fa5
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-07-22 21:23:18 -0300

    Aula 05: Describe / It

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+it.skip('A external test...', () => {
+    
+})
+
+describe('Should group tests...', () => {
+    describe('Should group more specific tests...', () => {
+        it('A specific test...', () => {
+            
+        })
+    })
+
+    describe('Should group more specific tests 2...', () => {
+        it('A specific test 2 ...', () => {
+            
+        })
+    })
+
+    it.skip('A internal test...', () => {
+        
+    })
+})
