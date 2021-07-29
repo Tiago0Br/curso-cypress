91d9cb8fe32c5a370a0350034159cf2c21c3ec52
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-07-28 21:36:43 -0300

    Aula 10: Textos

---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    it('Text', () => {
+        cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+        cy.get('body').should("contain", 'Cuidado')
+        // cy.get('body').should("have.text", 'Cuidado')
+        cy.get('span').should("contain", 'Cuidado')
+        // cy.get('div').should("contain", 'Cuidado')
+        cy.get('.facilAchar').should("have.text", 'Cuidado onde clica, muitas armadilhas...')
+    })
+})
