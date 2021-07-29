35db54ae8facdbcbaf39bf03df4957a5e17c6a8f
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-07-29 20:14:59 -0300

    Aula 13: Campos de Texto

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -27,4 +27,25 @@ describe('Work with basic elements', () => {
         cy.contains('Voltar').click()
         cy.get('#resultado').should("have.text", 'Voltou!')
     })
+
+    it('TextFields', () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should("have.value", 'Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should("have.value", 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', { delay: 100 })
+            .should('have.value', 'acerto')
+    })
 })
