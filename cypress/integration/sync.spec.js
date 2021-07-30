35776cf944be8af529fc1c6c52b499d12df95eca
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-07-29 22:43:33 -0300

    Aula 18: Entendendo as Esperas do Cypress

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Esperas...', () => {
+    before(() => {
+        cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Deve esperar elemento estar disponivel', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo').type('funciona')
+    })
+})
