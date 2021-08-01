7100a358fbef9ca81e6619c602c1b5732704df66
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-07-31 22:09:40 -0300

    Aula 23: Sincronizando o título

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -15,11 +15,15 @@ describe('Cypress basics', () => {
         cy.title().should("be.equal", 'Campo de Treinamento')
             .and("contain", 'Campo')
 
+        cy.title().should(title => {
+            console.log(title)
+        })
+
         //TODO imprimir o log no console
         //TODO escrever o log em um campo de texto
     })
 
-    it.only('Should find and interact with an element', () => {
+    it('Should find and interact with an element', () => {
         cy.visit('http://www.wcaquino.me/cypress/componentes.html')
 
         // cy.get('nao existe')
