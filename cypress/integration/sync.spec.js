f429dd4cc69e2e12a2187314317d20df53879114
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-07-31 22:15:02 -0300

    Aula 24: Should Vs Then

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -61,4 +61,13 @@ describe('Esperas...', () => {
             .click()
             .should('have.value', '111')
     })
+
+    it('Should vs Then', () => {
+        cy.get('#buttonListDOM').then($el => {
+            // .should('have.length', 1)
+            // console.log($el)
+            expect($el).to.have.length(1)
+            cy.get('#buttonList')
+        })
+    })
 })
