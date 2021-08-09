dcaf6ad2a1c21ee37b80bd8b88ca5436eb4653ab
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-08-08 21:36:44 -0300

    Aula 27: Invoke

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -41,4 +41,18 @@ describe('Helpers...', () => {
         cy.visit('http://www.wcaquino.me/cypress/componentes.html')
         cy.title().its('length').should('be.equal', 20)
     })
+
+    it('Invoke...', () => {
+        const getValue = () => 1
+        const soma = (a, b) => a + b
+
+        cy.wrap({ fn: getValue }).invoke('fn').should('be.equal', 1)
+        cy.wrap({ fn: soma }).invoke('fn', 2, 5).should('be.equal', 7)
+
+        cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+        cy.get('#formNome').invoke('val', 'Texto via invoke')
+        cy.window().invoke('alert', 'Dá pra ver?')
+        cy.get('#resultado')
+            .invoke('html', '<input type="button" value="hacked!" />')
+    })
 })
