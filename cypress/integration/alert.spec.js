5c50824f8eac9a91fab849df83ac5555c740aa33
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-08-15 17:19:48 -0300

    Aula 33: Confirm

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -23,4 +23,25 @@ describe('Work with alerts', () => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
         })
     })
+
+    it('Confirm', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it('Deny', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
+    })
 })
