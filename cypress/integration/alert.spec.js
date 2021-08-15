24da954b07b25fe75f4842d5a0b75f3c768df0f2
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-08-15 17:56:55 -0300

    Aula 34: Prompt

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -44,4 +44,17 @@ describe('Work with alerts', () => {
         })
         cy.get('#confirm').click()
     })
+
+    it('Prompt', () => {
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns('42')
+        })
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Era 42?')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':D')
+        })
+        cy.get('#prompt').click()
+    })
 })
