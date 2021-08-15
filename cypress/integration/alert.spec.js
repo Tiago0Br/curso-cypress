58d89972760ff3a278f068cac6b9fc5702fb40d0
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-08-15 18:36:16 -0300

    Aula 35: Desafio: Validar mensagens

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -57,4 +57,24 @@ describe('Work with alerts', () => {
         })
         cy.get('#prompt').click()
     })
+
+    it('Validando mensagens', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+
+        cy.get('#formNome').type('Wagner')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+        cy.get('[data-cy=dataSobrenome]').type('Aquino')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+    })
 })
