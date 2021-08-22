c8ec009d05e06914ca435896ed5dc77bbe4db0ab
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-08-22 16:52:12 -0300

    Aula 39: Popup via links

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -9,4 +9,31 @@ describe('Work with Popup', () => {
         cy.get('#buttonPopUp').click()
         cy.get('@winOpen').should('be.called')
     })
+
+    describe('With links...', () => {
+        beforeEach(() => {
+            cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+        })
+
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'http://www.wcaquino.me/cypress/frame.html')
+        })
+
+        it('Should access popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield').type('funciona')
+            })
+        })
+
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+            cy.get('#tfield').type('funciona')
+        })
+    })
 })
