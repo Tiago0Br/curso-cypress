c5de6e418bd461c3526556fa493c8ca1b6d85504
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-08-29 21:55:21 -0300

    Aula 41: Locators

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -9,7 +9,11 @@ describe('Work with basic elements', () => {
         cy.reload()
     })
 
-    it('...', () => {
-        
+    it('using jquery selector', () => {
+        cy.get(':nth-child(1) > :nth-child(3) > [type=button]')
+        cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input')
+        cy.get('[onclick*=Francisco]')
+        cy.get('#tabelaUsuarios td:contains(\'Doutorado\'):eq(0) ~ td:eq(3) > input')
+        cy.get('#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) input')
     })
 })
