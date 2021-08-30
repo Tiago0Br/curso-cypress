89c65de3624fad302e015d47833844e9d7edcdcb
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-08-29 22:08:48 -0300

    Aula 43: Xpath

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -18,6 +18,9 @@ describe('Work with basic elements', () => {
     })
 
     it('using xpath', () => {
-        cy.xpath('//input')
+        cy.xpath('//input[contains(@onclick, \'Francisco\')]')
+        cy.xpath("//table[@id='tabelaUsuarios']//td[contains(., 'Francisco')]/..//input[@type='text']")
+        cy.xpath("//td[contains(., 'Usuario A')]/following-sibling::td[contains(., 'Mestrado')]/..//input[@type='text']")
+            .type('funciona')
     })
 })
