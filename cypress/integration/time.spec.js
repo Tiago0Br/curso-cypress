8f89129dfc64ccf8621774a3f4cc94efed5dda8f
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-09-07 18:36:18 -0300

    Aula 48: Clock

---
+++ cypress/integration/time.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Work with alerts', () => {
+    before(() => {
+        cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    it('Going back to the past', () => {
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '07/11/2019')
+
+        // cy.clock()
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '31/12/1969')
+
+        const dt = new Date(2012, 3, 10, 15, 23, 50)
+        cy.clock(dt.getTime())
+        cy.get('#buttonNow').click()
+        cy.get('#resultado > span').should('contain', '10/04/2012')
+    })
+})
