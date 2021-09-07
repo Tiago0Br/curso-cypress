ad5e9c22e4d9875e63284d016d369383d3520062
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-09-07 19:48:03 -0300

    Aula 49: Tick

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -18,4 +18,24 @@ describe('Work with alerts', () => {
         cy.get('#buttonNow').click()
         cy.get('#resultado > span').should('contain', '10/04/2012')
     })
+
+    it('Goes to the future', () => {
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').should('contain', '15731')
+        // cy.get('#resultado > span').invoke('text').should('be.gt', 157317970)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(n => cy.wrap(Number(n)).should('be.lte', 0))
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').invoke('text').should('be.lte', 1000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(n => cy.wrap(Number(n)).should('be.gte', 5000))
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(n => cy.wrap(Number(n)).should('be.gte', 15000))
+    })
 })
