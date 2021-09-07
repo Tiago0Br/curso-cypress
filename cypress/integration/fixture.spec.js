bcd1ec9271ef89f0cab5a06cf9abc6032bc0ee0f
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-09-07 16:53:30 -0300

    Aula 44: Fixture

---
+++ cypress/integration/fixture.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Fixtures tests', () => {
+    it('Get data from fixture file', function() {
+        cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+        cy.fixture('userData').as('usuario').then(() => {
+            cy.get('#formNome').type(this.usuario.nome)
+            cy.get('#formSobrenome').type(this.usuario.sobrenome)
+            cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click()
+            cy.get(`[name=formComidaFavorita][value=${this.usuario.comida}]`).click()
+            cy.get('#formEscolaridade').select(this.usuario.escolaridade)
+            cy.get('#formEsportes').select(this.usuario.esportes)
+        })
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+    })
+})
