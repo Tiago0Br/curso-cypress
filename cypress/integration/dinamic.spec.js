a28934a0909112ab962df23021bf171738b292bf
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-09-07 17:48:03 -0300

    Aula 47: Each

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -9,16 +9,31 @@ describe('Dinamic tests', () => {
 
     foods.forEach(food => {
         it(`Cadastro com a comida ${food}`, () => {
-            cy.fixture('userData').as('usuario').then(() => {
-                cy.get('#formNome').type('Usuario')
-                cy.get('#formSobrenome').type('Qualquer')
-                cy.get(`[name=formSexo][value=F`).click()
-                cy.xpath(`//label[contains(., '${food}')]/preceding-sibling::input`).click()
-                cy.get('#formEscolaridade').select('Doutorado')
-                cy.get('#formEsportes').select('Corrida')
-                cy.get('#formCadastrar').click()
-                cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+            cy.get('#formNome').type('Usuario')
+            cy.get('#formSobrenome').type('Qualquer')
+            cy.get(`[name=formSexo][value=F`).click()
+            cy.xpath(`//label[contains(., '${food}')]/preceding-sibling::input`).click()
+            cy.get('#formEscolaridade').select('Doutorado')
+            cy.get('#formEsportes').select('Corrida')
+            cy.get('#formCadastrar').click()
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+        })
+
+        it('Deve selecionar todos usando o each', () => {
+            cy.get('#formNome').type('Usuario')
+            cy.get('#formSobrenome').type('Qualquer')
+            cy.get(`[name=formSexo][value=F`).click()
+            cy.get('[name=formComidaFavorita]').each($el => {
+                // $el.click()
+                if ($el.val() !== 'vegetariano')
+                    cy.wrap($el).click()
             })
+
+            cy.get('#formEscolaridade').select('Doutorado')
+            cy.get('#formEsportes').select('Corrida')
+            cy.get('#formCadastrar').click()
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+            // cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
         })
     })
 })
